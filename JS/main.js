05d0803af32f93d8273de0ca987dc9709ecc211f
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2021-09-30 13:44:27 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,13 @@ function slidChanger(x) {
     document.querySelector(`.sliderSection .row:nth-child(${x}) .indicator:nth-child(${currentSlid})`).classList.remove("active");
 
     currentSlid = x;
+    if (currentSlid > 2) {
+        document.querySelector(`.topOval`).style = "display:block;";
+        document.querySelector(`.circle`).style = "display:none;";
+    } else {
+        document.querySelector(`.topOval`).style = "display:none;";
+        document.querySelector(`.circle`).style = "display:block;";
+    }
 
     document.querySelector(`.sliderSection .row:nth-child(${currentSlid})`).classList.add("active");
     document.querySelector(`.sliderSection .row:nth-child(${currentSlid}) .indicator:nth-child(${currentSlid})`).classList.add("active");
