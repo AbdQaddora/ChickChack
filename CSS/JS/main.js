9bcc542dbd39c4b32635437e8342aa8e8c66a9a6
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2021-09-30 13:32:53 +0300

    distribute SASS code into many files

---
+++ CSS/JS/main.js
@@ -0,0 +1,20 @@
+let currentSlid = 1;
+document.querySelector(`.sliderSection .row:nth-child(${currentSlid})`).classList.add("active");
+document.querySelector(`.sliderSection .row:nth-child(${currentSlid}) .indicator:nth-child(${currentSlid})`).classList.add("active");
+
+function slidChanger(x) {
+    document.querySelector(`.sliderSection .row:nth-child(${currentSlid})`).classList.remove("active");
+    document.querySelector(`.sliderSection .row:nth-child(${x}) .indicator:nth-child(${currentSlid})`).classList.remove("active");
+
+    currentSlid = x;
+    if (currentSlid > 2) {
+        document.querySelector(`.topOval`).style = "display:block;";
+        document.querySelector(`.circle`).style = "display:none;";
+    } else {
+        document.querySelector(`.topOval`).style = "display:none;";
+        document.querySelector(`.circle`).style = "display:block;";
+    }
+
+    document.querySelector(`.sliderSection .row:nth-child(${currentSlid})`).classList.add("active");
+    document.querySelector(`.sliderSection .row:nth-child(${currentSlid}) .indicator:nth-child(${currentSlid})`).classList.add("active");
+}
